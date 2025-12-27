54e9530aa8bc28aef3e45a6abad0e1efc3372595
Keisyaaz <Keisyaazahra260@gmail.com>
2025-12-27 10:34:38 +0700

    fix vite assets in blade

--- resources/views/layout/app.blade.php
+++ resources/views/layout/app.blade.php
@@ -1,14 +1,19 @@
 <!DOCTYPE html>
-<html>
+<html lang="en">
 <head>
-    <title>@yield('title')</title>
-    <link rel="stylesheet" href="{{ asset('css/app.css') }}">
+    <meta charset="UTF-8">
+    <title>@yield('title', 'FitGym')</title>
+
+    @vite(['resources/css/app.css', 'resources/js/app.js'])
 </head>
 <body>
 
 <nav style="padding: 10px; background:#eee;">
-    <a href="/products">Produk</a> |
-    <a href="/logout">Logout</a>
+    <a href="/produk">Produk</a> |
+    <form action="/logout" method="POST" style="display:inline;">
+        @csrf
+        <button type="submit">Logout</button>
+    </form>
 </nav>
 
 <div class="container">
